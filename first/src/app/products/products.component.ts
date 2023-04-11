7ce3e644daa9ccbbe11578a70edad887e5920703
Jpeterson2012 <zerogravity@gmail.com>
2023-04-11 09:22:58 -0400

    initial angular practice commit

---
+++ first/src/app/products/products.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+  constructor() {}
+
+ngOnInit(): void {
+  
+}
+products = [
+  {id: 1, name: 'Minimalists Analog Watch', price: '109', color: 'Black', available: 'Available', image: '/assets/image1.jpg'},
+  {id: 2, name: 'Hisense Ultra HD Smart Tv', price: '900', color: 'Black', available: 'UnAvailable', image: 'assets/image2.jpg'},
+  {id: 3, name: 'Apple Iphone 12', price: '1744', color: 'White', available: 'Available', image: 'assets/image3.jpg'}
+];
+}
